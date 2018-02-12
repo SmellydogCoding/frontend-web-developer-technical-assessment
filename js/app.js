c3f788e254d15b3aca0fcf9a17ddf56a03a65d47
SmellydogCoding <SmellydogCoding@gmail.com>
2018-02-12 11:55:53 -0500

    lightbox arrow function

--- js/app.js
+++ js/app.js
@@ -14,22 +14,31 @@ function launchModal(event) {
   event.preventDefault();
   document.getElementsByTagName("body")[0].classList.add("noScroll");
   let overlay = document.getElementById("overlay");
-  overlay.innerHTML = "";
-
-  const imageList = buildImageList();
-  let startIndex = event.target.parentNode.parentNode.attributes["data-index"].nodeValue;
-  let imageBlock = `<figure>
-                      <div class="imageWrap">
-                        <span class="arrow prev">&#8249;</span>
-                        <img src="${imageList[startIndex].url}" alt="${imageList[startIndex].title}">
-                        <span class="arrow next">&#8250;</span>
-                      </div>
-                      <figcaption>${imageList[startIndex].title}</figcaption>
-                    </figure>`
-
-  overlay.innerHTML = imageBlock;
+  // overlay.innerHTML = "";
+  const list = buildImageList();
+  let index = parseInt(event.target.parentNode.parentNode.attributes["data-index"].nodeValue);
+  // let imageBlock = `<figure class="lightbox" data-index="${startIndex}">
+  // <div class="image-wrap">
+  // <span class="arrow prev">&#8249;</span>
+  // <img class="image" src="${imageList[startIndex].url}" alt="${imageList[startIndex].title}">
+  // <span class="arrow next">&#8250;</span>
+  // </div>
+  // <figcaption class="caption">${imageList[startIndex].title}</figcaption>
+  // </figure>`
+  // overlay.innerHTML = imageBlock;
+  loadPicture(index,list)
   overlay.classList.remove("hidden");
   
+  document.getElementsByClassName("prev")[0].addEventListener("click", function() {
+    index - 1 < 0 ? index = list.length - 1 : index --
+    console.log(index)
+    loadPicture(index, list);
+  });
+  document.getElementsByClassName("next")[0].addEventListener("click", function() {
+    index + 1 > list.length - 1 ? index = 0 : index ++
+    loadPicture(index, list);
+  });
+  closeLightboxListener();
 }
 
 function buildImageList() {
@@ -37,7 +46,6 @@ function buildImageList() {
   let imageList = [];
   for (i = 0; i < images.length; i++) {
     let image = {
-      // "index": images[i].attributes["data-index"].nodeValue,
       "title": images[i].innerText,
       "url": images[i].firstElementChild.attributes.href.nodeValue
     }
@@ -46,15 +54,34 @@ function buildImageList() {
   return imageList;
 }
 
+function loadPicture(index,imageList) {
+  document.getElementsByClassName("image")[0].src = imageList[index].url;
+  document.getElementsByClassName("image")[0].alt = imageList[index].title;
+  document.getElementsByClassName("caption")[0].innerHTML = imageList[index].title;
+}
+
+function closeLightboxListener() {
+  document.addEventListener("click", function(event) {
+    let image = document.getElementsByClassName("image")[0];
+    let lightbox = document.getElementsByClassName("lightbox")[0];
+    let overlay = document.getElementById("overlay");
+    if(event.target === lightbox || event.target === overlay) {
+      overlay.classList.add("hidden")
+      document.getElementsByTagName("body")[0].classList.remove("noScroll");
+    };
+  });
+}
+
 function init() {
   const links = document.getElementsByClassName("photo-link");
   for (let l = 0; l < links.length; l++) {
     links[l].addEventListener("click", launchModal);
   };
-  document.getElementById("overlay").addEventListener("click", function() {
-    overlay.classList.add("hidden");
-    document.getElementsByTagName("body")[0].classList.remove("noScroll");
-  });
+  // document.addEventListener("click", function(event) {
+  //   let image = document.getElementsByClassName("image")[0];
+  //   if(!image.contains(event.target)) { overlay.classList.add("hidden") };
+  //   document.getElementsByTagName("body")[0].classList.remove("noScroll");
+  // });
 }
 
 window.onload = function() { 
