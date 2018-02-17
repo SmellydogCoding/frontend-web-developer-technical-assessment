e9c6072f3c0bbf36697b763a81416f8a0424d481
SmellydogCoding <SmellydogCoding@gmail.com>
2018-02-17 13:24:26 -0500

    smooth out animations

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// get images from Flickr
 function loadPics() {
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -10,16 +11,24 @@ function loadPics() {
   xhttp.send();
 }
 
-function launchModal(event) {
+function launchLightbox(event) {
   event.preventDefault();
+  document.getElementsByClassName("image-wrap")[0].style.width = Math.floor(window.innerWidth * .5) + "px";
+  document.getElementsByClassName("image-wrap")[0].style.height = Math.floor(window.innerHeight * .5) + "px";
   document.getElementsByTagName("body")[0].classList.add("noScroll");
   let overlay = document.getElementById("overlay");
+  overlay.classList.add("fadein");
   overlay.classList.remove("hidden");
   
   let index = parseInt(event.target.parentNode.parentNode.attributes["data-index"].nodeValue);
   const list = buildImageList();
   
-  loadImage(index, list);
+  // loadImage(index, list);
+  let image = document.getElementsByClassName("image")[0];
+  image.src = list[index].url;
+  image.onload = function() {
+    formatImage(index, list);
+  }
 
   document.getElementsByClassName("prev")[0].addEventListener("click", function() {
     index - 1 < 0 ? index = list.length - 1 : index --
@@ -30,7 +39,7 @@ function launchModal(event) {
     index + 1 > list.length - 1 ? index = 0 : index ++
     fadeout(index, list);
   });
-
+  
   closeLightboxListener();
 }
 
@@ -61,11 +70,11 @@ function loadImage(index, imageList) {
 
 
 function fadeout(index, imageList){
-	let opacity = 1;
+  let opacity = 1;
 	let image = document.getElementsByClassName("image")[0];
 	let timer = setInterval(function() {
-		if (opacity <= 0) {
-			clearInterval(timer);
+    if (opacity <= 0) {
+      clearInterval(timer);
       image.style.height = "";
       image.style.width = "";
 			loadImage(index, imageList);
@@ -83,51 +92,73 @@ function formatImage(index, imageList) {
   let maxWidth = Math.floor(window.innerWidth * .75);
   let maxHeight = Math.floor(window.innerHeight * .85);
   if (imageHeight > maxHeight) {
-    image.style.height = maxHeight + "px";
-    image.style.width = Math.floor(maxHeight * ratio) + "px";
     imageHeight = maxHeight;
     imageWidth = Math.floor(maxHeight * ratio);
   }
   if (imageWidth > maxWidth) {
-    image.style.width = maxWidth + "px";
+    resizeLightboxWidth(maxWidth)
     imageWidth = maxWidth;
-    image.style.height = Math.floor(maxWidth / ratio) + "px";
     imageHeight = Math.floor(maxWidth / ratio);
   }
-  formatLightbox(index,imageList,imageHeight,imageWidth);
+  resizeLightboxWidth(imageWidth,imageHeight,index,imageList)
 }
 
-function formatLightbox(index, imageList, imageHeight, imageWidth) {
+function resizeLightboxWidth(imageWidth,imageHeight,index,imageList) {
   let lightbox = document.getElementsByClassName("image-wrap")[0];
   let lightboxWidth = lightbox.clientWidth;
-  let lightboxHeight = lightbox.clientHeight;
-  
-  let lightboxAnimation = setInterval(function() {
-    
+  let resizeLightboxAnimation = setInterval(function() {
+    if (imageWidth > lightboxWidth) { 
+      if (imageWidth - lightboxWidth < 10) { lightboxWidth += (imageWidth - lightboxWidth); }
+      else { lightboxWidth += 10; }
+      lightbox.style.width = lightboxWidth + "px"; 
+    }
     if (imageWidth < lightboxWidth) {
-      lightboxWidth -= 1; 
+      if(lightboxWidth - imageWidth < 10) { lightboxWidth -= (lightboxWidth - imageWidth); }
+      else { lightboxWidth -= 10; }
       lightbox.style.width = lightboxWidth + "px"; 
-    } else if (imageWidth > lightboxWidth) { 
-      lightboxWidth += 1; 
-      lightbox.style.width = lightboxWidth + "px"; 
-    }
-    
-    if (imageHeight < lightboxHeight) { 
-      lightboxHeight -= 1; 
-      lightbox.style.height = lightboxHeight + "px"; 
-    } else if (imageHeight > lightboxHeight) { 
-      lightboxHeight += 1; 
-      lightbox.style.height = lightboxHeight + "px"; 
-    }
-    
-    if (imageWidth === lightboxWidth && imageHeight === lightboxHeight) { 
-      clearInterval(lightboxAnimation);
-      fadein(index, imageList);
+    } 
+    if (imageWidth === lightboxWidth) { 
+      clearInterval(resizeLightboxAnimation);
+      resizeLightboxHeight(imageWidth,imageHeight,index,imageList)
     }
   }, 1);
 }
 
-function fadein(index, imageList) {
+function resizeLightboxHeight(imageWidth,imageHeight,index,imageList) {
+  let lightbox = document.getElementsByClassName("image-wrap")[0];
+  let lightboxHeight = lightbox.clientHeight;
+  let resizeLightboxAnimation = setInterval(function() {
+    if (imageHeight > lightboxHeight) { 
+      if (imageHeight - lightboxHeight < 10) { lightboxHeight += (imageHeight - lightboxHeight); }
+      else { lightboxHeight += 10; }
+      lightbox.style.height = lightboxHeight + "px"; 
+    }
+    if (imageHeight < lightboxHeight) { 
+      if (lightboxHeight - imageHeight < 10) { lightboxHeight -= (lightboxHeight - imageHeight); }
+      else { lightboxHeight -= 10; }
+      lightbox.style.height = lightboxHeight + "px"; 
+    } 
+    if (imageHeight === lightboxHeight) { 
+      clearInterval(resizeLightboxAnimation);
+      loadImageData(imageWidth,imageHeight,index,imageList);
+    }
+  }, 1);
+}
+
+function loadImageData(imageWidth,imageHeight,index,imageList) {
+  let image = document.getElementsByClassName("image")[0];
+  let link = document.getElementsByClassName("flickr-link")[0]
+  image.alt = imageList[index].title
+  document.getElementsByClassName("title")[0].innerHTML = imageList[index].title;
+  link.href = imageList[index].link;
+  link.innerHTML = "View Photo on Flickr"
+  image.style.width = imageWidth + "px";
+  image.style.height = imageHeight + "px";
+  fadein(imageWidth,imageHeight,index,imageList);
+}
+
+function fadein(imageWidth,imageHeight,index,imageList) {
+  let image = document.getElementsByClassName("image")[0];
   let opacity = 0;  // initial opacity
   let timer = setInterval(function() {
     if (opacity >= 1) { clearInterval(timer); }
@@ -135,7 +166,6 @@ function fadein(index, imageList) {
     opacity += 0.1;
   }, 30);
 }
-  
 
 function closeLightboxListener() {
   document.addEventListener("click", function(event) {
@@ -147,9 +177,17 @@ function closeLightboxListener() {
       overlay.classList.add("fadeout");
       setTimeout(function() {
         overlay.classList.add("hidden");
+        overlay.classList.remove("fadein");
+        overlay.classList.remove("fadeout");
+        image.src = "";
+        image.alt = "";
+        image.style.cssText = "";
+        document.getElementsByClassName("image-wrap")[0].style.cssText = "";
+        document.getElementsByClassName("title")[0].innerHTML = "";
+        document.getElementsByClassName("flickr-link")[0].href = "";
+        document.getElementsByClassName("flickr-link")[0].innerHTML = "";
         document.getElementsByTagName("body")[0].classList.remove("noScroll");
       }, 500);
-      document.getElementsByClassName("image")[0].classList.add("fadeout");
     };
   });
 }
@@ -157,7 +195,7 @@ function closeLightboxListener() {
 function init() {
   const links = document.getElementsByClassName("photo-link");
   for (let l = 0; l < links.length; l++) {
-    links[l].addEventListener("click", launchModal);
+    links[l].addEventListener("click", launchLightbox);
   };
 }
 
